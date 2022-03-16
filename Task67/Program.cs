a6790be4032e45e8b6d087671249a4164a828f7a
Daniil <dtsikin@mail.ru>
2022-03-16 22:48:10 +0500

    Complete Task67

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+void Numbers(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{m}");
+        Fill(m,n);
+    }
+
+}
+void Fill(int m,int n)
+{
+    while(m < n)
+    {
+        m++;
+        Console.Write($", {m} ");
+    }
+
+}
+
+Numbers(1, 5);
+Console.WriteLine();
+Numbers(4, 8);
